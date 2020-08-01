430d103b04b81a1f76d93213c5f10b97056c03a6
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-01 17:26:21 -0500

    Got Ex working

--- .emacs.d/startup/21-evil.el
+++ .emacs.d/startup/21-evil.el
@@ -1,2 +1,13 @@
 (use-package evil)
 (evil-mode)
+(evil-ex-define-cmd "Ex" 'dired-jump)
+
+(evil-ex-define-cmd "eval" 'my/eval-buffer-elisp)
+
+(defun my/vex () "Port of the Vex command from vim" 
+  (split-window-right)
+  (windmove-right)
+  (dired-jump))
+
+(evil-ex-define-cmd "Vex" 'my/vex)
+
