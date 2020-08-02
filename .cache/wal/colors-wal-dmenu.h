173f8244f796ef6caeb64a6afc12e6a66a08b78c
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-01 19:14:03 -0500

    Removes unnecessary files from config

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f7f7fb", "#282936" },
-	[SchemeSel] = { "#f7f7fb", "#ea51b2" },
-	[SchemeOut] = { "#f7f7fb", "#a1efe4" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#282936";
-static const char* selfgcolor   = "#f7f7fb";
-static const char* normbgcolor  = "#00f769";
-static const char* normfgcolor  = "#f7f7fb";
-static const char* urgbgcolor   = "#ea51b2";
-static const char* urgfgcolor   = "#f7f7fb";
