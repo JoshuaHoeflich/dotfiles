67e8808c5ef85bbb463ebccfa77b375a8b34e9bb
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-08-07 21:29:18 -0500

    Begins refactoring out overlays

--- .nix-defexpr/default.nix
+++ .nix-defexpr/default.nix
@@ -2,48 +2,5 @@
 buildEnv {
   name = "user-env";
   extraOutputsToInstall = [ "out" "bin" "lib" ];
-  paths = [
-    autocutsel
-    bat
-    bitwarden
-    dash
-    deepin.deepin-screenshot
-    direnv
-    dmenu
-    emacs27
-    exa
-    feh
-    gcc
-    git
-    go
-    google-chrome
-    htop
-    kitty
-    lf
-    lorri
-    neofetch
-    neovim
-    nerdfonts
-    nix
-    nodejs-12_x
-    pamixer
-    pavucontrol
-    picom
-    python38Full
-    pywal
-    ranger
-    ripgrep
-    rnix-lsp
-    rustup
-    spotify
-    stack
-    terraform-lsp
-    unzip
-    watchexec
-    xclip
-    xlockmore
-    xmobar
-    xmonad-with-packages
-    zsh
-  ];
+  paths = [ packageList ];
 }
