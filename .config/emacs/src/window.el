a257af07cebab31bee80265638f476a5be547de5
Joshua Hoeflich <josh@joshuahoeflich.com>
2020-12-17 07:15:46 -0600

    Removes effectively unused code in window.el

--- .config/emacs/src/window.el
+++ .config/emacs/src/window.el
@@ -17,23 +17,3 @@
     (shrink-window-horizontally (or amount 5))))
 
 (global-set-key (kbd "C-c h") 'window/push-border-left)
-
-(defun window/push-border-down (&optional amount)
-  "Push the border AMOUNT down."
-  (interactive)
-  (let ((window-on-top (= 0 (car (window-edges)))))
-    (if window-on-top
-	    (enlarge-window (or amount 5))
-      (shrink-window (or amount 5)))))
-
-(global-set-key (kbd "C-c C-j") 'window/push-border-down)
-
-(defun window/push-border-up (&optional amount)
-  "Push the border AMOUNT up."
-  (interactive)
-  (let ((window-on-top (= 0 (car (window-edges)))))
-    (if window-on-top
-	    (shrink-window (or amount 5))
-      (enlarge-window (or amount 5)))))
-
-(global-set-key (kbd "C-c C-k") 'window/push-border-up)
