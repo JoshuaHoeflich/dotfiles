35fd2377c69ac87a89c3e7b9337a22a6b0c81e2c
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-10 10:49:58 -0600

    Fix issue with LISP mode and sly

--- .config/emacs/packages/languages/lisp.el
+++ .config/emacs/packages/languages/lisp.el
@@ -1,6 +1,3 @@
-(use-package sly
-  :mode "\\.lisp\\'")
-
 (use-package racket-mode
   :mode "\\.rkt\\'")
 
@@ -12,3 +9,11 @@
 (add-hook 'sly-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'racket-mode-hook #'rainbow-delimiters-mode)
+
+(use-package sly)
+(defun jlib/lisp-mode-hook ()
+  "Hook for Common LISP files."
+  (interactive)
+  (rainbow-delimiters-mode)
+  (sly))
+(add-hook 'lisp-mode-hook #'jlib/lisp-mode-hook)
--- .config/emacs/vanilla/alist.el
+++ .config/emacs/vanilla/alist.el
@@ -1,8 +1,9 @@
 (add-to-list 'default-frame-alist '(font . "Inconsolata:size=24"))
-;; (add-to-list 'display-buffer-alist
-;;              `("sly-mrepl"
-;;                (display-buffer-no-window)
-;;                (allow-no-window . t)))
+(add-to-list 'auto-mode-alist '("\\.lisp\\'" . lisp-mode))
+(add-to-list 'display-buffer-alist
+             `("sly-mrepl"
+               (display-buffer-no-window)
+               (allow-no-window . t)))
 ;; (add-to-list 'display-buffer-alist
 ;;              `("Racket REPL"
 ;;                (display-buffer-below-selected display-buffer-at-bottom)
