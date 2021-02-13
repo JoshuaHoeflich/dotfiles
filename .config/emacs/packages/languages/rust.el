921c60730fa6946f9fee6bf132cf57838fe51983
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-13 11:13:59 -0600

    Further improvements to rust.el

--- .config/emacs/packages/languages/rust.el
+++ .config/emacs/packages/languages/rust.el
@@ -1,9 +1,11 @@
 (use-package rust-mode)
 (use-package flycheck-rust)
+
 (defun jlib/rust-hook ()
   "Hook for entering Rust mode."
   (interactive)
-  (flycheck-rust-setup))
+  (flycheck-rust-setup)
+  (eglot-ensure))
 
 (add-hook 'rust-mode-hook 'jlib/rust-hook)
 
