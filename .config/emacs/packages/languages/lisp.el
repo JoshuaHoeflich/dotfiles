27f60f0684df681035a30dca32709eb217e7ef91
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-15 08:15:00 -0600

    Removes unnecessary config from lisp.el

--- .config/emacs/packages/languages/lisp.el
+++ .config/emacs/packages/languages/lisp.el
@@ -18,25 +18,11 @@
   (seq-find (lambda (buf) (string-prefix-p "*sly-mrepl" (buffer-name buf)))
             (buffer-list)))
 
-(defun jlib/load-lisp ()
-  "When my current project has a current system.lisp, load it."
-  (let ((system-path
-         (jlib/path-join (jlib/get-current-project)
-                         "src" "repl-wm" "system.lisp")))
-    (when system-path
-      (sly-load-file system-path))))
-
-(add-hook 'sly-connected-hook 'jlib/load-lisp)
-
-(defun jlib/boot-sly ()
-  "Start and load SLY when it is appropriate to do so."
-  (unless (jlib/sly-already-open-p)
-    (sly)))
-
 (defun jlib/lisp-mode-hook ()
   "Hook for Common LISP files."
   (interactive)
   (rainbow-delimiters-mode)
-  (jlib/boot-sly))
+  (unless (jlib/sly-already-open-p)
+    (sly)))
 
 (add-hook 'lisp-mode-hook #'jlib/lisp-mode-hook)
