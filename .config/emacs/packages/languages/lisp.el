4e3fb6e3782011242c943cf6291b4a175aa97c7a
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-14 20:03:34 -0600

    Begins improving sly workflow

--- .config/emacs/packages/languages/lisp.el
+++ .config/emacs/packages/languages/lisp.el
@@ -17,11 +17,26 @@
   (interactive)
   (seq-find (lambda (buf) (string-prefix-p "*sly-mrepl" (buffer-name buf)))
             (buffer-list)))
+
+(defun jlib/load-lisp ()
+  "When my current project has a current system.lisp, load it."
+  (let ((system-path
+         (jlib/path-join (jlib/get-current-project)
+                         "src" "repl-wm" "system.lisp")))
+    (when system-path
+      (sly-load-file system-path))))
+
+(add-hook 'sly-connected-hook 'jlib/load-lisp)
+
+(defun jlib/boot-sly ()
+  "Start and load SLY when it is appropriate to do so."
+  (unless (jlib/sly-already-open-p)
+    (sly)))
+
 (defun jlib/lisp-mode-hook ()
   "Hook for Common LISP files."
   (interactive)
   (rainbow-delimiters-mode)
-  (unless (jlib/sly-already-open-p)
-    (sly)))
+  (jlib/boot-sly))
 
 (add-hook 'lisp-mode-hook #'jlib/lisp-mode-hook)
